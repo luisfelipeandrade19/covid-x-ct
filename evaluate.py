02ecb6a9545e6811e1ed94077b76e09c3e4bd3ef
Felipe <luisfelipe7014feups@gmail.com>
2026-05-29 10:49:01 -0300

    feat: Implementation of training curve

--- evaluate.py
+++ evaluate.py
@@ -1,6 +1,8 @@
 import os
+from pathlib import Path
 
 import matplotlib.pyplot as plt
+import pandas as pd
 import seaborn as sns
 import torch
 from sklearn.metrics import classification_report, confusion_matrix
@@ -47,27 +49,73 @@ if __name__ == "__main__":
         yticklabels=["Normal", "Pneumonia", "COVID-19"],
     )
     plt.title("Matriz de Confusão")
-    plt.xlabel('Predição')
-    plt.ylabel('Real')
+    plt.xlabel("Predição")
+    plt.ylabel("Real")
     plt.tight_layout()
     plt.savefig("matriz-confusao.png")
     plt.show()
 
     # Matriz confusão normalizada
-    cm_norm = confusion_matrix(todas_labels, todas_preds, normalized='true')
-    plt.figure(figsize=(8,6))
+    cm_norm = confusion_matrix(todas_labels, todas_preds, normalized="true")
+    plt.figure(figsize=(8, 6))
     sns.heatmap(
         cm_norm,
         annot=True,
         fmt="d",
         cmap="Blues",
         xticklabels=["Normal", "Pneumonia", "COVID-19"],
-        yticklabels=["Normal", "Pneumonia", "COVID-19"]
+        yticklabels=["Normal", "Pneumonia", "COVID-19"],
     )
     plt.title("Matriz de Confusão Normalizada")
-    plt.xlabel('Predição')
-    plt.ylabel('Real')
+    plt.xlabel("Predição")
+    plt.ylabel("Real")
     plt.tight_layout()
     plt.savefig("matriz-confusao-normalizada.png")
     plt.show()
 
+    # Curvas de Treinamento
+
+    # ALERTA: Sempre trocar a versão quando treinar novamente
+    metrics_path = (
+        Path(Config.BASE_PATH) / "lightning_csv_logs" / "version_0" / "metrics.csv"
+    )
+    metrics = pd.read_csv(metrics_path)
+
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 5))
+
+    # Loss
+    ax1.plot(
+        metrics["epoch"].dropna().unique(),
+        metrics.grouphy("epoch")["train_loss"].mean().dropna(),
+        label="Treino",
+    )
+    ax1.plot(
+        metrics["epoch"].dropna().unique(),
+        metrics.grouphy("epoch")["val_loss"].mean().dropna(),
+        label="Validação",
+    )
+    ax1.set_xlabel("Epoca")
+    ax1.set_ylabel("Loss")
+    ax1.set_title("Evolução da Loss")
+    ax1.legend()
+    ax1.grid(True, alpha=0.3)
+
+    # Acuracy
+    ax2.plot(
+        metrics["epoch"].dropna().unique(),
+        metrics.grouphy("epoch")["train_acc"].mean().dropna(),
+        label="Treino",
+    )
+    ax1.plot(
+        metrics["epoch"].dropna().unique(),
+        metrics.grouphy("epoch")["val_acc"].mean().dropna(),
+        label="Validação",
+    )
+    ax1.set_xlabel("Epoca")
+    ax1.set_ylabel("Loss")
+    ax1.set_title("Evolução da Acuracy")
+    ax1.legend()
+    ax1.grid(True, alpha=0.3)
+
+    plt.tight_layout()
+    plt.save_fig("curvas_treinamento.png", dpi=300)
