27b50d140dd79ed84f238b5fb38dc3fe41d37ee4
Felipe <luisfelipe7014feups@gmail.com>
2026-07-28 11:44:43 -0300

    feat: Clean

--- clean_outputs.py
+++ clean_outputs.py
@@ -4,10 +4,18 @@ from config import Config
 
 def remove_dir(dir_path):
     if os.path.exists(dir_path):
-        print(f"Limpando diretório: {dir_path}")
-        shutil.rmtree(dir_path)
+        print(f"Limpando o conteúdo do diretório: {dir_path}")
+        for filename in os.listdir(dir_path):
+            file_path = os.path.join(dir_path, filename)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+            except Exception as e:
+                print(f"Falha ao apagar {file_path}. Razão: {e}")
     else:
-        print(f"Diretório já está limpo ou não existe: {dir_path}")
+        print(f"Diretório não existe, ignorando: {dir_path}")
 
 def clean_all_experiments():
     """
