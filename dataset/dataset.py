100de2499f8d64cad5914381d6aac3693788b37c
Felipe <luisfelipe7014feups@gmail.com>
2026-07-29 17:33:13 -0300

    Removendo coarse do data augmentation

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -141,7 +141,6 @@ train_transforms = A.Compose([
     A.Affine(translate_percent=(-0.05, 0.05), scale=(0.9, 1.1), rotate=(-15, 15), p=0.5),
     A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=0.5),
     A.GaussianBlur(blur_limit=(3, 7), p=0.3),
-    A.CoarseDropout(num_holes_range=(1, 8), hole_height_range=(8, 16), hole_width_range=(8, 16), fill=0, p=0.3),
     A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
     ToTensorV2()
 ])
